a4093ed0b10ca6b7b5245d6a688c7b6f574f4f60
mnhtndev <tuannguyen.02042005@gmail.com>
2026-03-21 14:38:46 +0700

    add: merge and correct data

--- roads-map.js
+++ roads-map.js
@@ -14,51 +14,152 @@ require([
     container: "viewDiv",
     map: map,
     extent: new Extent({
-      xmin: 105.6,
-      ymin: 9.8,
-      xmax: 106.5,
-      ymax: 10.6,
+      xmin: 102,
+      ymin: 8,
+      xmax: 110,
+      ymax: 23,
       spatialReference: { wkid: 4326 }
     })
   });
 
-  const expresswayLayer = new GeoJSONLayer({
-    url: "./data/road/CT_TL_MT__MT_CT.json",
-    title: "Các tuyến cao tốc Trung Lương - Mỹ Thuận - Cần Thơ",
-    outFields: ["*"],
-    renderer: {
-      type: "simple",
-      symbol: {
-        type: "simple-line",
-        color: [214, 61, 57, 0.95],
-        width: 4.5,
-        style: "solid"
-      }
-    },
-    popupTemplate: {
-      title: "{name}",
-      content: function(event) {
-        const attrs = event.graphic.attributes || {};
-        const lengthKm = typeof attrs.lenght === "number" ? attrs.lenght : Number(attrs.lenght || 0);
-        const rightOfWay = typeof attrs.lo_gioi === "number" ? attrs.lo_gioi : Number(attrs.lo_gioi || 0);
-        const provinceValue = attrs.province;
-        const provinceText = Array.isArray(provinceValue)
-          ? provinceValue.join(", ")
-          : (provinceValue || "Không có dữ liệu");
+  const roadFiles = [
+    "bacgiang-langson.json",
+    "danang-quangngai.json",
+    "HCM-longthanhDaugiay_HCM-trungluong.json",
+    "quynhon-chithanh.json",
+    "trungluong-mythuan_mythuan-cantho.json"
+  ];
 
-        return "<b>Chiều dài:</b> " + lengthKm.toLocaleString("vi-VN") + " km<br/>" +
-          "<b>Lộ giới:</b> " + rightOfWay.toLocaleString("vi-VN") + " m<br/>" +
-          "<b>Tỉnh/TP đi qua:</b> " + provinceText;
-      }
+  function normalizeFeature(feature, fileName) {
+    const geometry = feature.geometry;
+    const geometryType = geometry && geometry.type;
+    if (geometryType !== "LineString" && geometryType !== "MultiLineString") {
+      return null;
     }
-  });
 
-  map.add(expresswayLayer);
+    const attrs = feature.properties || {};
+    const roadName = attrs.name || fileName.replace(".json", "");
+    const rawLength = attrs.length != null ? attrs.length : attrs.lenght;
+    const rawRightOfWay = attrs.lo_gioi != null ? attrs.lo_gioi : attrs["lo gioi"];
+    const rawProvince = attrs.province != null ? attrs.province : attrs.provinces;
 
-  const legend = new Legend({
-    view: view
+    const parsedLength = Number(rawLength);
+    const parsedRightOfWay = Number(rawRightOfWay);
+
+    const lengthText = Number.isFinite(parsedLength)
+      ? parsedLength.toLocaleString("vi-VN") + " km"
+      : "Không có dữ liệu";
+
+    const rightOfWayText = Number.isFinite(parsedRightOfWay)
+      ? parsedRightOfWay.toLocaleString("vi-VN") + " m"
+      : "Không có dữ liệu";
+
+    const provinceText = Array.isArray(rawProvince)
+      ? rawProvince.join(", ")
+      : (rawProvince || "Không có dữ liệu");
+
+    return {
+      type: "Feature",
+      geometry: geometry,
+      properties: {
+        name: roadName,
+        length_text: lengthText,
+        right_of_way_text: rightOfWayText,
+        province_text: provinceText
+      }
+    };
+  }
+
+  async function loadAllRoadData() {
+    const datasets = await Promise.all(
+      roadFiles.map(async function(fileName) {
+        const response = await fetch("./data/road/" + fileName);
+        const data = await response.json();
+        return {
+          fileName: fileName,
+          features: Array.isArray(data.features) ? data.features : []
+        };
+      })
+    );
+
+    const mergedFeatures = datasets.flatMap(function(item) {
+      return item.features.map(function(feature) {
+        return normalizeFeature(feature, item.fileName);
+      }).filter(function(feature) {
+        return feature !== null;
+      });
+    });
+
+    const mergedGeoJson = {
+      type: "FeatureCollection",
+      features: mergedFeatures
+    };
+
+    const colorPalette = [
+      [214, 61, 57, 0.95],
+      [34, 139, 34, 0.95],
+      [30, 144, 255, 0.95],
+      [255, 140, 0, 0.95],
+      [138, 43, 226, 0.95],
+      [0, 139, 139, 0.95],
+      [178, 34, 34, 0.95],
+      [70, 130, 180, 0.95]
+    ];
+
+    const roadNames = Array.from(new Set(mergedFeatures.map(function(feature) {
+      return feature.properties.name;
+    })));
+
+    const uniqueValueInfos = roadNames.map(function(roadName, index) {
+      return {
+        value: roadName,
+        label: roadName,
+        symbol: {
+          type: "simple-line",
+          color: colorPalette[index % colorPalette.length],
+          width: 4.5,
+          style: "solid"
+        }
+      };
+    });
+
+    const blob = new Blob([JSON.stringify(mergedGeoJson)], { type: "application/json" });
+    const blobUrl = URL.createObjectURL(blob);
+
+    const expresswayLayer = new GeoJSONLayer({
+      url: blobUrl,
+      title: "Cac tuyen duong",
+      outFields: ["*"],
+      renderer: {
+        type: "unique-value",
+        field: "name",
+        uniqueValueInfos: uniqueValueInfos,
+        defaultSymbol: {
+          type: "simple-line",
+          color: [120, 120, 120, 0.9],
+          width: 4.5,
+          style: "solid"
+        }
+      },
+      popupTemplate: {
+        title: "{name}",
+        content: "<b>Chiều dài:</b> {length_text}<br/>" +
+          "<b>Lộ giới:</b> {right_of_way_text}<br/>" +
+          "<b>Tỉnh/TP đi qua:</b> {province_text}"
+      }
+    });
+
+    map.add(expresswayLayer);
+
+    expresswayLayer.when(function() {
+      URL.revokeObjectURL(blobUrl);
+    });
+
+  }
+
+  loadAllRoadData().catch(function(error) {
+    console.error("Khong the tai toan bo du lieu road", error);
   });
-  view.ui.add(legend, "bottom-right");
 
 });
 
--- universities-map.js
+++ universities-map.js
@@ -14,9 +14,90 @@ require([
   const view = new MapView({
     container: "viewDiv",
     map: map,
-    zoom: 17,
-    center: [106.6948371, 10.7829634]
+    zoom: 6,
+    center: [106.2, 10.5]
   });
 
-  
+  const universitiesLayer = new GraphicsLayer({
+    title: "Diem truong dai hoc"
+  });
+  map.add(universitiesLayer);
+
+  const dataFiles = [
+    "./data/university/ueh_hcmue.json",
+    "./data/university/uit_ctu.json",
+    "./data/university/agu_tgu.json"
+  ];
+
+  const universitySymbol = {
+    type: "picture-marker",
+    url: "https://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png",
+    width: "28px",
+    height: "28px"
+  };
+
+  function toUniversityGraphic(university) {
+    const longitude = Number(university.longitude);
+    const latitude = Number(university.latitude);
+
+    if (!Number.isFinite(longitude) || !Number.isFinite(latitude)) {
+      return null;
+    }
+
+    return new Graphic({
+      geometry: new Point({
+        longitude: longitude,
+        latitude: latitude
+      }),
+      symbol: universitySymbol,
+      attributes: {
+        name: university.name || "Khong co du lieu",
+        year: university.year || "Khong ro",
+        majors: university.majors || "Khong co du lieu",
+        students: Number(university.students) || 0
+      },
+      popupTemplate: {
+        title: "{name}",
+        content: function(event) {
+          const attrs = event.graphic.attributes || {};
+          return "<b>Nam thanh lap:</b> " + attrs.year + "<br/>" +
+            "<b>Nganh dao tao:</b> " + attrs.majors + "<br/>" +
+            "<b>So luong sinh vien (tuong doi):</b> Khoang " +
+            Number(attrs.students).toLocaleString("vi-VN") + " sinh vien";
+        }
+      }
+    });
+  }
+
+  function loadUniversityData() {
+    const requests = dataFiles.map(function(url) {
+      return esriRequest(url, {
+        responseType: "json"
+      }).then(function(response) {
+        return Array.isArray(response.data) ? response.data : [];
+      });
+    });
+
+    Promise.all(requests)
+      .then(function(results) {
+        const universities = results.flat();
+        const graphics = universities
+          .map(toUniversityGraphic)
+          .filter(function(graphic) { return graphic !== null; });
+
+        universitiesLayer.addMany(graphics);
+
+        if (graphics.length > 0) {
+          view.goTo(graphics).catch(function() {
+            // Keep default view if goTo fails.
+          });
+        }
+      })
+      .catch(function(error) {
+        console.error("Khong the tai du lieu truong dai hoc", error);
+      });
+  }
+
+  view.when(loadUniversityData);
+
 });
